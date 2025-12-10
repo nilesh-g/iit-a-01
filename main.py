4338571dd1197bbe1f1755a62c4b102dc78b33c9
Nilesh Ghule <nilesh@sunbeaminfo.com>
2025-12-10 09:50:37 +0530

    add subtract

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-
+import arithmetic
 print("Hello, World!")
+
+num1 = int(input("Enter number1 : "))
+num2 = int(input("Enter number2 : "))
+
+arithmetic.add(num1, num2)
+arithmetic.subtract(num1, num2)
